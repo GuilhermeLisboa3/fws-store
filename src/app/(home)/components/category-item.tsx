00840c342657adea222cede20b24555927f2a384
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-18 16:11:27 -0300

    feat: ensure user goes to catalog page on category page on category item click

--- src/app/(home)/components/category-item.tsx
+++ src/app/(home)/components/category-item.tsx
@@ -1,17 +1,20 @@
 import { Category } from "@prisma/client"
 import { Badge } from "@/components/ui/badge"
 import { CATEGORY_ICON } from "@/constants/category-icons"
+import Link from "next/link"
 
 interface CategoryItemProps {
   category: Category
 }
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
-  return ( 
+  return (
+  <Link href={`/category/${category.slug}`}>
     <Badge className="flex justify-center items-center gap-2 rounded-lg py-3" variant="outline">
       {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
       <span className="text-xs font-bold">{category.name}</span>
     </Badge>
+  </Link> 
   )
 }
 
