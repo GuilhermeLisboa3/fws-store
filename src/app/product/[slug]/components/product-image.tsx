fc493bcb415953a75518c1a5bae5b0c0199ff1d0
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-19 13:19:10 -0300

    feat: add ProductImage component

---
+++ src/app/product/[slug]/components/product-image.tsx
@@ -0,0 +1,42 @@
+'use client'
+import Image from "next/image"
+import { useState } from "react"
+
+interface ProductImagesProps {
+  name: string
+  imageUrls: string[]
+}
+
+const ProductImages = ({ imageUrls, name }: ProductImagesProps) => {
+  const [currentImage, setCurrentImage] = useState(imageUrls[0])
+
+  const handleImageClick = (imageUrl: string) => {
+    setCurrentImage(imageUrl)
+  }
+
+  return ( 
+    <div className="flex flex-col">
+      <div className="flex h-[380px] w-full items-center justify-center bg-accent">
+        <Image src={currentImage} alt={name} width={0} height={0} sizes="100vw"
+        className="h-auto max-h-[70%] w-auto max-w-[80%]"
+        style={{ objectFit: 'contain' }}/>
+      </div>
+
+      <div className="grid grid-cols-4 gap-4 mt-8 px-5">
+        {imageUrls.map(imageUrl => (
+          <button key={imageUrl} className={`flex items-center justify-center rounded-lg bg-accent h-[100px]
+            ${imageUrl === currentImage && "border-2 border-solid border-primary"}
+          `}
+            onClick={() => handleImageClick(imageUrl)}
+          >
+            <Image src={imageUrl} alt={name} width={0} height={0} sizes="100vw"
+            className="h-auto max-h-[70%] w-auto max-w-[80%]"
+            style={{ objectFit: 'contain' }}/>
+          </button>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default ProductImages
